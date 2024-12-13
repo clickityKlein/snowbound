c1c1149a4bde0cc743284ee0a53d50e82dbb834d
Carl Klein <carlj.klein@gmail.com>
2024-12-12 19:43:53 -0700

    app.py updated with correct port

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from snowbound import app
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=10000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+    # app.run(debug=True)
